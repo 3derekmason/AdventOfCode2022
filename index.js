d9d7bed3ac99961ab35ae6f2a9bfbc7f1b8ba79f
Derek Mason <3derekmason@gmail.com>
2022-12-04 13:53:49 -0700

    refactor: use console.table to display results

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import myTotalScore from "./day2.js";
 import sumOfPriorities from "./day3.js";
 import overlapingPairs from "./day4.js";
 
-console.log("Day 1", carryingMostCalories());
-console.log("Day 2", myTotalScore());
-console.log("Day 3", sumOfPriorities());
-console.log("Day 4", overlapingPairs());
+const data = [
+  { day: 1, solution: carryingMostCalories() },
+  { day: 2, solution: myTotalScore() },
+  { day: 3, solution: sumOfPriorities() },
+  { day: 4, solution: overlapingPairs() },
+];
+
+console.table(data);
