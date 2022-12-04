cc238bfb3d765a287d2feab1e6c88bbae0a55a59
Derek Mason <3derekmason@gmail.com>
2022-12-04 10:20:23 -0700

    build: day 4 init

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import carryingMostCalories from "./day1.js";
 import myTotalScore from "./day2.js";
 import sumOfPriorities from "./day3.js";
+import overlapingPairs from "./day4.js";
 
 console.log("Day 1", carryingMostCalories());
 console.log("Day 2", myTotalScore());
 console.log("Day 3", sumOfPriorities());
+console.log("Day 4", overlapingPairs());
