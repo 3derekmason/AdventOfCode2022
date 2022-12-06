28d678958d94353a9806de932b1569a9cefbad2e
Derek Mason <3derekmason@gmail.com>
2022-12-05 18:18:27 -0700

    build: day 5 init

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import carryingMostCalories from "./day1.js";
 import myTotalScore from "./day2.js";
 import sumOfPriorities from "./day3.js";
 import overlapingPairs from "./day4.js";
+import postRearrangement from "./day5.js";
 
 const data = [
   { day: 1, solution: carryingMostCalories() },
   { day: 2, solution: myTotalScore() },
   { day: 3, solution: sumOfPriorities() },
   { day: 4, solution: overlapingPairs() },
+  { day: 5, solution: postRearrangement() },
 ];
 
 console.table(data);
